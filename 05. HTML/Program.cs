448a9eb7c74d46de4ca3167668ab8756f441824b
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-12 20:54:22 +0200

    Add files via upload

---
+++ 05. HTML/Program.cs
@@ -0,0 +1,31 @@
+﻿string article = Console.ReadLine();
+string content = Console.ReadLine();
+
+List<string> listComments = new List<string>();
+
+while (true)
+{
+    string comment = Console.ReadLine();
+
+	if (comment == "end of comments")
+	{
+		break;
+	}
+
+	listComments.Add(comment);
+}
+
+Console.WriteLine("<h1>");
+Console.WriteLine(article);
+Console.WriteLine("</h1>");
+
+Console.WriteLine("<article>");
+Console.WriteLine(content);
+Console.WriteLine("</article>");
+
+foreach (var comment in listComments)
+{
+	Console.WriteLine("<div>");
+	Console.WriteLine(comment);
+	Console.WriteLine("</div>");
+}
