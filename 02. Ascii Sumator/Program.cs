8c49cda4e83f680b41ad63accf67b61fc466c1e6
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-12 16:27:54 +0200

    Add files via upload

---
+++ 02. Ascii Sumator/Program.cs
@@ -0,0 +1,20 @@
+﻿char first = char.Parse(Console.ReadLine());
+char second = char.Parse(Console.ReadLine());
+string chars = Console.ReadLine();
+
+int start = Math.Min(first, second);
+int end = Math.Max(first,second);
+
+string result = string.Empty;
+int sum = 0;
+
+for (int i = 0; i < chars.Length; i++)
+{
+	int ch = chars[i];
+	if (ch > start && ch < end)
+	{
+		sum += ch;
+	}
+}
+
+Console.WriteLine(sum);
